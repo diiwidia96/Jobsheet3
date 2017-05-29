4ac983fc0d0ff0d55402fad3a03d33e1920641ad
diiwidia96 <ronasahila96@gmail.com>
2017-05-29 15:16:38 +0700

    Add files via upload

---
+++ HurufAB.php
@@ -0,0 +1,31 @@
+<html><head><title>Statement IF</title></head>
+<body>
+<?php echo"<form action=\"$php_selt\" method=\"post\">";
+$nilail=$_POST['nilail'];
+echo"Berapa Nilai PHPmu? :
+<input name=\"nilail\" value=\"$nilail\" size=\3\" type=\"text\"value=\>;
+<input name=\"cek\" type=\"submit\" value=\"Cek\"> </form>";
+if($_POST['cek']) //perintah untuk menguji penekanan tombol Cek
+{echo"<br><hl>";
+	if($nilail=="")
+		{echo"Anda Belum Menentukan Nilai..!";}
+	elseif(!is_numeric($nilail)) //jika nilail tidak bertype numerik
+ 		{echo" Ini Bukan Nilai Angka";}
+	else
+		{
+			if($nilail<=30)		// statement pertama
+				{ echo"Anda mendapat nilai huruf <b> E </b>"; }
+			elseif($nilail<=50) // statement kedua
+				{ echo"Anda mendapat nilai huruf <b> D </b>"; }
+			elseif($nilail<=70) // statement ketiga
+				{ echo"Anda mendapat nilai huruf <b> C </b>"; }
+			elseif($nilail<=85) // statement keempat
+				{ echo"Anda mendapat nilai huruf <b> AB </b>"; }
+			else				// statement kelima
+				{echo"Anda mendapat nilai huruf <b> A </b>"; }
+		}
+	echo"</hl>";
+}
+?>
+</body>
+</html>
---
+++ namabulan.php
@@ -0,0 +1,25 @@
+<html>
+<head><title>Peryataan SWITCH</title></head>
+<body>
+<?php
+$m=date("m", time()) ; // mencari nilai bulan dalam format m
+switch ($m)
+	{
+	case "1" : $namabulan="Janauri";break;
+	case "2" : $namabulan="Febuari";break;
+	case "3" : $namabulan="Maret";break;
+	case "4" : $namabulan="April";break;
+	case "5" : $namabulan="Mei";break;
+	case "6" : $namabulan="Juni";break;
+	case "7" : $namabulan="Juli";break;
+	case "8" : $namabulan="Agustus";break;
+	case "9" : $namabulan="September";break;
+	case "10" : $namabulan="Oktober";break;
+	case "11" : $namabulan="November";break;
+	case "12" : $namabulan="Desember";break;
+	}
+echo"<h2>Sekarang adalah bulan : $namabulan</h2> ";
+?>
+</body>
+</html>
+	
